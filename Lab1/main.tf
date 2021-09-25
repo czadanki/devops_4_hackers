c465368c7a4f4e70b3852ea8d4df071772018681
Mariusz Czadankiewicz <czaanki@gmail.com>
2021-09-25 01:49:10 +0000

    Firewall added

--- Lab1/main.tf
+++ Lab1/main.tf
@@ -18,3 +18,42 @@ resource "digitalocean_droplet" "devops-vm-1" {
     digitalocean_ssh_key.ssh_key.fingerprint
   ]
 }
+
+resource "digitalocean_firewall" "firewall" {
+  name = "devops-vm-1"
+
+  droplet_ids = [digitalocean_droplet.devops-vm-1.id]
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["143.198.104.91/32"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["143.198.104.91/32"]
+  }
+
+    inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["143.198.104.91/32"]
+  }
+
+    inbound_rule {
+    protocol         = "tcp"
+    port_range       = "7443"
+    source_addresses = ["143.198.104.91/32"]
+  }
+  outbound_rule {
+    protocol         = "udp"
+    port_range       = "1-65535"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+  outbound_rule {
+    protocol         = "tcp"
+    port_range       = "1-65535"
+    destination_addresses = ["0.0.0.0/0"]
+  }
+} 
